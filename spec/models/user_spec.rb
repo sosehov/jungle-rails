aee42ea7d424ad13555fac4219c5cb599a6941a0
Soseh <sosehv@gmail.com>
2025-05-30 12:00:44 -0400

    Adds model specs for products

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "is valid with a password and password confirmation"
+
+    it "is not valid without a password and password confirmation"
+
+    it ""
+    
+end
