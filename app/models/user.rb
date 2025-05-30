c119b89d3877912aab55d59d10bd01d032d7bc4e
Soseh <sosehv@gmail.com>
2025-05-30 13:26:48 -0400

    Adds model spec:user and updates neccessary files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  def self.authenticate_with_credentials(email, password)
+    normalized_email = email.strip.downcase
+    user = User.find_by('LOWER(email) = ?', normalized_email)
+    user&.authenticate(password) ? user : nil
+  end
 end
